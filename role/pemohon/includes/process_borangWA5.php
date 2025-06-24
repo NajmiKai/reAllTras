1b57eae173932a03324ee3b977393bd5dbb82997
Najmi Khairuzzaman <yunonajmi@gmail.com>
2025-06-25 03:39:15 +0800

    wilayahAsal can now be done every year

--- role/pemohon/includes/process_borangWA5.php
+++ role/pemohon/includes/process_borangWA5.php
@@ -4,7 +4,6 @@ include '../../../includes/config.php';
 require '../../../PHPMailer/src/Exception.php';
 require '../../../PHPMailer/src/PHPMailer.php';
 require '../../../PHPMailer/src/SMTP.php';
-include '../../../includes/config.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
